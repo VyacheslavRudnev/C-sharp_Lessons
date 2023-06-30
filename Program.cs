5932dc3b281893d4b65bbe18e32629d74424586f
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-06-30 19:51:41 +0300

    task05 done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,57 @@
-﻿namespace ConsoleApp2
+﻿using System.Text;
+
+namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.InputEncoding = Encoding.Unicode;
+                Console.OutputEncoding = Encoding.Unicode;
+                Console.WriteLine("Practice/ex05");
+                Console.WriteLine();
+
+                Console.Write("Вкажіть перше число: A = ");
+                int A = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Вкажіть друге число: B = (має бути більшим за А)");
+                int B = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                if (A < B)
+                {
+                    for (int i = A; i <= B; i++)
+                    {
+                        for (int j = 0; j < i; j++)
+                        {
+                            Console.Write(i +" ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Число B має бути більшим за A.");
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Помилка формату введення! Має бути число, а не текст!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                Console.ReadKey();
+                Console.ResetColor();
+            }
         }
     }
 }
