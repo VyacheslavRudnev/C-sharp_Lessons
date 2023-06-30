401c703791c4f401bce3ed9f18602242a7072a57
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-06-30 23:19:06 +0300

    HW task03 done

--- Program.cs
+++ Program.cs
@@ -11,6 +11,14 @@ namespace ConsoleApp2
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
 
+            Console.WriteLine("Введіть чотири цифри: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int num4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            int number = num1 * 1000 + num2 * 100 + num3 * 10 + num4;
+            Console.WriteLine("Сформование число: " + number);
             
             Console.WriteLine();
             Console.ReadKey();
