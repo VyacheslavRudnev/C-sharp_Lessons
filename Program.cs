c29c7ce57bf4fb98fe4e603ec190af2c9d9bdd53
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-06-30 18:11:11 +0300

    task01 done

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Practice/ex01");
+            Console.WriteLine();
+            Console.WriteLine("It's easy to win forgiveness for being wrong;");
+            Console.WriteLine("being right is what gets you into real trouble.");
+            Console.WriteLine("Bjarne Stroustrup");
+            Console.WriteLine();
         }
     }
 }
