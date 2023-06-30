e7fba046d986ede7c299275c97c40f3b8b41829e
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-06-30 23:58:36 +0300

    HW task07 done

--- Program.cs
+++ Program.cs
@@ -6,12 +6,28 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Practice/ex06");
+            Console.WriteLine("HW/ex07");
             Console.WriteLine();
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
 
-            
+            Console.WriteLine("Введіть перше число:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введіть друге число:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int start = Math.Min(number1, number2);
+            int end = Math.Max(number1, number2);
+
+            if (start % 2 != 0)
+                start++;
+
+            Console.WriteLine("Парні числа у вказаному діапазоні: ");
+            for (int i = start; i <= end; i += 2)
+            {
+                Console.Write($" {i}");
+            }
             Console.WriteLine();
             Console.ReadKey();
         }
