00c367987b470ef2bc272827ca311b46289b39b0
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-06-30 23:12:52 +0300

    HW task02 done

--- Program.cs
+++ Program.cs
@@ -6,12 +6,19 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Practice/ex06");
+            Console.WriteLine("HW/ex02");
             Console.WriteLine();
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
 
-            
+            Console.WriteLine("Введіть два числа. Де перше число буде значенням, а друге - выдсоток першого числа. ");
+            Console.Write("Введіть перше число: ");
+            int value = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введіть друге число: ");
+            int percent = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            int result = value * percent / 100;
+            Console.WriteLine("Відсоток числа {0} від числа {1} = {2}", percent, value, result);
             Console.WriteLine();
             Console.ReadKey();
         }
