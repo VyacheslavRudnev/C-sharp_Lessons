938239eb67f4a03d1ed5bf1292e3d04702e58e87
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-06-30 23:00:38 +0300

    HW task01 done

--- Program.cs
+++ Program.cs
@@ -6,14 +6,56 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Practice/ex06");
+            Console.WriteLine("HW/ex01");
             Console.WriteLine();
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
-
             
-            Console.WriteLine();
-            Console.ReadKey();
+            try
+            {
+                Console.Write("Введіть число від 1 до 100: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                if(number < 1 || number > 100)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Помилка! Введене число не відповідає вказаному діапазону");
+                    return;
+                }
+                if (number % 3 == 0 && number % 5 != 0)
+                {
+                   Console.WriteLine("Fizz");
+                }
+                else if (number % 3 != 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                {
+                    Console.WriteLine($"Число {number}");
+                }
+                
+            }
+            catch (FormatException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Помилка формату введення! Має бути число, а не текст!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                Console.ReadKey();
+                Console.ResetColor();
+            }
+ 
         }
     }
 }
