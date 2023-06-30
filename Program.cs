d2628867bba3c171680a36df7d93624a4d139191
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-06-30 22:39:17 +0300

    dev remaster

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ConsoleApp2
+﻿using System.Text;
+
+namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Practice/ex06");
+            Console.WriteLine();
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
