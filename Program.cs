45d6614fb54145346b0ee0db40a5b968bd44c8ea
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-07-01 16:17:18 +0300

    Practice/Mod02/task02 done

--- Program.cs
+++ Program.cs
@@ -6,12 +6,41 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Practice/ex06");
+            Console.WriteLine("Practice/Mod02/ex02");
             Console.WriteLine();
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
 
-            
+            Console.Write("Вкажіть розмір масиву: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Вкажіть параметр для пошуку: ");
+            int key = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Random rnd = new Random();
+            int[] arr = new int[size];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(-10, 20);
+            }
+            Console.WriteLine("Похідний масив: ");
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Елементи масиву меньше ({key}):"); 
+            int countElem = 0;
+            foreach (var i in arr)
+            {
+                if(i<key)
+                {
+                    Console.Write(i + " ");
+                    countElem++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Кількість елементів масиву < {key}: {countElem}");
             Console.WriteLine();
             Console.ReadKey();
         }
