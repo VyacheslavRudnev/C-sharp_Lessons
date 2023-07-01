8fa6a366d4540f23ec769acdee7f3dd81d934db3
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-07-01 17:08:14 +0300

    Practice/Mod02/task03 done

--- Program.cs
+++ Program.cs
@@ -11,69 +11,49 @@ namespace ConsoleApp2
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
 
-            Console.Write("Вкажіть розмір масиву: ");
-            int size = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Вкажіть три цифри в диапазоні (-10, 20): ");
-            int key1 = Convert.ToInt32(Console.ReadLine());
-            int key2 = Convert.ToInt32(Console.ReadLine());
-            int key3 = Convert.ToInt32(Console.ReadLine());
-                     
-            Console.WriteLine();
-            Random rnd = new Random();
-            int[] arr = new int[size];
+            Console.WriteLine("Введіть три числа, розділені пробілами:");
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = rnd.Next(-10, 20);
-            }
-            Console.WriteLine("Похідний масив: ");
-            foreach (int i in arr)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine();
-            int count1 = 0,
-                count2 = 0,
-                count3 = 0;
-            
-            foreach (var i in arr)
-            {
-                if (i == key1)
-                {
-                    count1++;
-                }
-                else if (i == key2)
-                {
-                    count2++;
-                }
-                else if (i == key3)
-                {
-                    count3++;
-                }
-            }
-            int count = 0;
-            if (count1 < count2 && count1 < count3)
-            {
-                count = count1;
-            }
-            else if (count2 < count1 && count2 < count3)
-            {
-                count = count2;
-            }
-            else if (count3 < count1 && count3 < count2)
-            {
-                count = count3;
-            }
-            Console.WriteLine();
-            Console.WriteLine($"Кількість повторень послідовностей для {key1}: {count1}");
-            Console.WriteLine($"Кількість повторень послідовностей для {key2}: {count2}");
-            Console.WriteLine($"Кількість повторень послідовностей для {key3}: {count3}");
-            Console.WriteLine($"Кількість повторень послідовностей для всіх чисел: {count}");
-           
+            int[] array = { 7, 6, 5, 3, 4, 7, 6, 5, 8, 7, 6, 5 };
+            int count = CountSequence(array, numbers);
 
-
+            Console.WriteLine("Кількість повторень послідовності: " + count);
             Console.WriteLine();
             Console.ReadKey();
+        }
+        static int CountSequence(int[] array, string[] sequence)
+        {
+            int count = 0;
+            int index = 0;
+
+            while (index < array.Length - sequence.Length + 1)
+            {
+                bool found = true;
+
+                for (int i = 0; i < sequence.Length; i++)
+                {
+                    int number = int.Parse(sequence[i]);
+
+                    if (array[index + i] != number)
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+
+                if (found)
+                {
+                    count++;
+                    index += sequence.Length;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+
+            return count;
         }
     }
 }
