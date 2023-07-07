3d8a4af2aa009dfc733b4fa770ab03aed8571fba
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-07-07 14:43:33 +0300

    task03 done

--- Program.cs
+++ Program.cs
@@ -6,20 +6,27 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Practice/Mod02/ex03");
-            Console.WriteLine();
+//Завдання 3
+//Користувач вводить з клавіатури три числа. Необхідно
+//підрахувати кількість разів послідовності цих трьох чисел
+//в масиві.
+//Наприклад:
+//користувач ввів: 7 6 5;
+//масив: 7 6 5 3 4 7 6 5 8 7 6 5;
+//кількість повторень послідовності: 3.
+
+            Console.WriteLine("Practice/Mod02/ex03\n");
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
 
-            Console.WriteLine("Введіть три числа, розділені пробілами:");
+            Console.Write("Введіть три числа, розділені пробілами: ");
             string input = Console.ReadLine();
             string[] numbers = input.Split(' ');
 
             int[] array = { 7, 6, 5, 3, 4, 7, 6, 5, 8, 7, 6, 5 };
             int count = CountSequence(array, numbers);
 
-            Console.WriteLine("Кількість повторень послідовності: " + count);
-            Console.WriteLine();
+            Console.WriteLine("\nКількість повторень послідовності: " + count);
             Console.ReadKey();
         }
         static int CountSequence(int[] array, string[] sequence)
