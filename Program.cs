455a86612593c8a1fce650d981a86230f9bf989f
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-07-07 23:44:47 +0300

    HW/task05 done

--- Program.cs
+++ Program.cs
@@ -6,12 +6,37 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Practice/ex06");
-            Console.WriteLine();
+//Завдання 5
+//Користувач з клавіатури вводить до рядка арифме -
+//тичний вираз.Додаток має підрахувати його результат.
+//Необхідно дотримуватися лише двох операцій: +і –.
+            Console.WriteLine("HW/Mod02/ex05\n");
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("Введіть арифметичний вираз: ");
+            string expression = Console.ReadLine();
+            int result = 0;
+            int i = 0;
+            while (i < expression.Length)
+            {
+                if (expression[i] == '+')
+                {
+                    result += int.Parse(expression[i + 1].ToString());
+                    i += 2;
+                }
+                else if (expression[i] == '-')
+                {
+                    result -= int.Parse(expression[i + 1].ToString());
+                    i += 2;
+                }
+                else
+                {
+                    result += int.Parse(expression[i].ToString());
+                    i++;
+                }
+            }
+            Console.WriteLine("Результат: " + result);
 
-            
             Console.WriteLine();
             Console.ReadKey();
         }
