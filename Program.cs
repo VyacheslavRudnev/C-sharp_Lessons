99d6cf2b3ae3fbb1b3199884da1761c269e659c6
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-07-08 12:19:09 +0300

    task06 done

--- Program.cs
+++ Program.cs
@@ -6,13 +6,17 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Practice/ex06");
-            Console.WriteLine();
+//Завдання 6
+//Користувач вводить речення з клавіатури.Вам необ-
+//хідно підрахувати кількість слів у ньому.
+            Console.WriteLine("Practice/Mod02/ex06\n");
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
+            Console.Write("Введіть речення: ");
+            string str = Console.ReadLine();
+            string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("\nКількість слів у реченні: " + words.Length);
 
-            
-            Console.WriteLine();
             Console.ReadKey();
         }
     }
