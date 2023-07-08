00686c0852b0c9242cea4b48426094857296c2a2
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-07-08 12:30:15 +0300

    task08 done

--- Program.cs
+++ Program.cs
@@ -6,13 +6,25 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Practice/ex06");
+//Завдання 8
+//Користувач вводить з клавіатури речення.Додаток має
+//підрахувати кількість голосних літер у реченні.
+            Console.WriteLine("Practice/Mod02/ex08\n");
             Console.WriteLine();
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("Введіть речення українською мовою: ");
+            string str = Console.ReadLine();
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'а' || str[i] == 'е' || str[i] == 'и' || str[i] == 'і' || str[i] == 'о' || str[i] == 'у' || str[i] == 'ю' || str[i] == 'я' || str[i] == 'є' || str[i] == 'ї')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("\nКількість голосних літер у реченні: " + count);
 
-            
-            Console.WriteLine();
             Console.ReadKey();
         }
     }
