d830518526642d518922248f3dc4415ce55ba44f
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-07-08 12:23:05 +0300

    task07 done

--- Program.cs
+++ Program.cs
@@ -6,13 +6,25 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Practice/ex06");
-            Console.WriteLine();
+//Завдання 7
+//Користувач вводить речення з клавіатури.Вам необхід-
+//но перевернути кожне слово речення і вивести результат
+//на екрані.
+            Console.WriteLine("Practice/Mod02/ex07\n");
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
+            Console.Write("Введіть речення: ");
+            string str = Console.ReadLine();
+            string[] words = str.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] word = words[i].ToCharArray();
+                Array.Reverse(word);
+                words[i] = new string(word);
+            }
+            str = string.Join(" ", words);
+            Console.WriteLine(str);
 
-            
-            Console.WriteLine();
             Console.ReadKey();
         }
     }
